5200c8c4317ba319c93c17f61cc087587ced490c
Amalya Megerman <amalyamegerman@gmail.com>
2023-01-06 15:33:05 -0500

    passes all tests

--- hash-table.js
+++ hash-table.js
@@ -1,4 +1,4 @@
-const sha256 = require('js-sha256');
+const sha256 = require("js-sha256");
 
 class KeyValuePair {
   constructor(key, value) {
@@ -9,32 +9,79 @@ class KeyValuePair {
 }
 
 class HashTable {
-
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(numBuckets);
+    this.data.fill(null, 0);
   }
 
   hash(key) {
-    // Your code here
+    //takes in string `key`
+    //returns first 8 chars of key's sha256 as an int
+
+    return parseInt(sha256(key).slice(0, 8), 16);
   }
 
   hashMod(key) {
-    // Your code here
+    // take in string `key`
+    //returns hash modulo of the capacity of the data array
+
+    return this.hash(key) % this.capacity;
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    let index = this.hashMod(key);
+
+    if (!this.data[index]) {
+      this.data[index] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      throw new Error("hash collision or same key/value pair already exists!");
+    }
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    let index = this.hashMod(key);
+    let keyVal = new KeyValuePair(key, value);
+
+    if (this.data[index]) {
+      keyVal.next = this.data[index];
+      this.data[index] = keyVal;
+    } else {
+      this.data[index] = keyVal;
+    }
+    this.count++;
   }
 
   insert(key, value) {
-    // Your code here
+    let keyVal = new KeyValuePair(key, value);
+    let index = this.hashMod(key);
+    let overwrite = false;
+
+    if (!this.data[index]) {
+      this.data[index] = keyVal;
+    } else {
+      //traverse the node, check if the new new KeyValuePair
+      //contains the same key as a pair already in the hash table
+      if (this.data[index].next) {
+        let current = this.data[index];
+        while (current) {
+          if (current.key === keyVal.key) {
+            current.value = keyVal.value;
+            this.count--;
+            overwrite = true;
+          }
+          current = current.next;
+        }
+      }
+      if (!overwrite) {
+        keyVal.next = this.data[index];
+        this.data[index] = keyVal;
+      }
+    }
+    this.count++;
   }
-
 }
-
 
 module.exports = HashTable;
